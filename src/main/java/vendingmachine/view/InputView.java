d8ed02bdaa462bf4364df6320670b7b6e13c04b5
SehyeonKang <gedfgrdt@naver.com>
2024-02-20 20:59:06 +0900

    feat: InputView 클래스 추가, 사용자 입력받는 기능 추가

---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,26 @@
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readMachineTotalAmount() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String readProductInfo() {
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String readInputAmount() {
+        System.out.println("투입 금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String readProductToPurchase() {
+        System.out.println("구매할 상품명을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
