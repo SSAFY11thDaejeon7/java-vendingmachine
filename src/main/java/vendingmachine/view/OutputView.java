6bdbfc0569c7b97bd9c9051b29eae15d12ba4911
SehyeonKang <gedfgrdt@naver.com>
2024-02-20 21:00:59 +0900

    feat: OutputView 클래스 추가, 출력 관련 기능 추가

---
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -0,0 +1,27 @@
+package vendingmachine.view;
+
+import vendingmachine.domain.Coin;
+
+import java.util.Map;
+
+public class OutputView {
+
+    public void printTotalCoins(Map<Coin, Integer> coinInfo) {
+        System.out.println("자판기가 보유한 동전");
+        for (Coin coin : coinInfo.keySet()) {
+            System.out.println(coin.getAmount() + "원 - " + coinInfo.get(coin) + "개");
+        }
+    }
+
+    public void printInputAmount(int inputAmount) {
+        System.out.println("투입 금액: " + inputAmount + "원");
+    }
+
+    public void printChange(Map<Coin, Integer> coinInfo) {
+        // TODO: DTO 만들어서 잔돈으로 반활할 수 있는 금액만 반환하도록 하기
+        System.out.println("자판기가 보유한 동전");
+        for (Coin coin : coinInfo.keySet()) {
+            System.out.println(coin.getAmount() + "원 - " + coinInfo.get(coin) + "개");
+        }
+    }
+}
