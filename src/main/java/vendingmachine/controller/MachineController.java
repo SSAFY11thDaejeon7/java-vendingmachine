51f5b146dc96ccfe4b9e0074ed89f2b5c50528de
SehyeonKang <gedfgrdt@naver.com>
2024-02-20 21:10:46 +0900

    feat: MachineController 클래스 추가

---
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -0,0 +1,36 @@
+package vendingmachine.controller;
+
+import vendingmachine.domain.RandomCoinGenerator;
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+import java.util.Map;
+
+public class MachineController {
+
+
+    private final OutputView outputView;
+    private final InputView inputView;
+    private final VendingMachine vendingMachine = new VendingMachine();
+
+    public MachineController(OutputView outputView, InputView inputView) {
+        this.outputView = outputView;
+        this.inputView = inputView;
+    }
+
+    public void play() {
+        int totalAmount = Integer.parseInt(inputView.readMachineTotalAmount());
+        RandomCoinGenerator generator = new RandomCoinGenerator();
+        Map<Integer, Integer> map = generator.generateRandomCoins(totalAmount);
+
+        for (int key : map.keySet()) {
+            System.out.println(key + "원: " + map.get(key) + "개");
+        }
+
+        String input = inputView.readProductInfo();
+        vendingMachine.addProducts(input);
+
+        System.out.println("check");
+    }
+}
